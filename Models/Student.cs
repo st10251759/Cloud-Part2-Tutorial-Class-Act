fdf5138fe39cae6dd1968e2d5163f47c982ddaf0
st10251759 <ST10251759@vcconnect.edu.za>
2024-04-16 14:12:27 +0200

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentApp.Models
+{
+    public class Student
+    {
+        //Table elementsa to include
+        //need to public to access from all tables
+        public int StudentId { get; set; }
+        //String is mean't to be nullable to build database 
+        public string? StudentName { get; set;}
+        public int Age { get; set;}
+    }
+}
